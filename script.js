a4effae271bdec130d6f9527bfbb945cf17d1069
showrav-ansary <showrav.ansary.bd@gmail.com>
2022-10-25 05:05:23 +0600

    implement balance sorting

--- script.js
+++ script.js
@@ -78,7 +78,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const displayMovements = function (movements, sort = false) {
     containerMovements.innerHTML = '';
-    movements.forEach(function (movement, index) {
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+    movs.forEach(function (movement, index) {
         const type = movement > 0 ? 'deposit' : 'withdrawal';
         const html = `
         <div class="movements__row">
@@ -217,3 +218,11 @@ btnClose.addEventListener('click', function(e){
     }
     inputCloseUsername.value = inputClosePin.value = '';
 }); 
+
+let sorted = false;
+
+btnSort.addEventListener('click', function(e){
+    e.preventDefault();
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+});
